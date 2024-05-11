04c1ec959984c517b4f61599d549b42fd416729b
Fwwlad <115725717+Fwwlad@users.noreply.github.com>
2024-05-11 13:51:42 +0300

    Create statuses.sql

---
+++ statuses.sql
@@ -0,0 +1,24 @@
+USE [SKstudio]
+GO
+
+INSERT INTO [dbo].[StatusesOfAppointments]
+           ([name])
+     VALUES
+           ('запланирована')
+GO
+INSERT INTO [dbo].[StatusesOfAppointments]
+           ([name])
+     VALUES
+           ('завершена')
+GO
+INSERT INTO [dbo].[StatusesOfAppointments]
+           ([name])
+     VALUES
+           ('отменена')
+GO
+INSERT INTO [dbo].[StatusesOfAppointments]
+           ([name])
+     VALUES
+           ('нерабочие часы')
+GO
+
